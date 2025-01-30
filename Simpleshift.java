d534f25a54d8a66bbfb37b526f614c97ff8c09ee
somaharini <somaharini24@gmail.com>
2025-01-30 21:44:19 +0530

    Create Simpleshift.java
    this program displays about the shift operation

---
+++ Simpleshift.java
@@ -0,0 +1,7 @@
+public class Simpleshift {
+    public static void main(String[] args) {
+        int number = 8;
+        System.out.println("Left shift (<< 1): " + (number << 1));
+        System.out.println("Right shift (>> 1): " + (number >> 1));
+    }
+}
